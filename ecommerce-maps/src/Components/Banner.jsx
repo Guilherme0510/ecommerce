e875ef90dc2638998be41f770a837cacc65f5871
Guilherme <guissilval005@gmail.com>
2024-11-12 08:39:54 -0300

    testando para ver se agora foi

--- ecommerce-maps/src/Components/Banner.jsx
+++ ecommerce-maps/src/Components/Banner.jsx
@@ -18,7 +18,7 @@ const Banner = () => {
           <div className="flex items-center gap-2">
             <p className="w-8 md:w-11 h-[2px] bg-[#6B4423]"></p>
             <p className="font-medium text-sm md:text-base">
-              NOSSOS MAIS VENDIDOS
+              NOSSOS MAIS VENDIDOS de nosso site
             </p>
           </div>
           <h1 className="prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed">
