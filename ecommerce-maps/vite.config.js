ec48c9790a0d6d33f9e6e055eadb68efb2bff544
Guilherme Silva <guissilval005@gmail.com>
2024-11-19 08:24:34 -0300

    Alterando as configs do vite para o deploy funcionar

--- ecommerce-maps/vite.config.js
+++ ecommerce-maps/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/', // Define a base para rotas relativas (use '/' para SPAs hospedadas na raiz)
+  build: {
+    outDir: 'dist', // Diretório de saída padrão
+  },
+  server: {
+    host: true, // Permite acessar em outros dispositivos na mesma rede
+  },
+});
