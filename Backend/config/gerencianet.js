18dcf79acb36d476f6cb05c4d34d36006e9ac6a0
Guilherme <guissilval005@gmail.com>
2024-11-22 16:37:44 -0300

    Arrumando o deploy do backend7

--- Backend/config/gerencianet.js
+++ Backend/config/gerencianet.js
@@ -1,16 +1,25 @@
 import EfiPay from 'sdk-node-apis-efi';
 import fs from 'fs';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url'; // Importar fileURLToPath para compatibilidade com módulos ES
 
 dotenv.config();
 
+// Obter o caminho do diretório com import.meta.url
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const gerenciaNetConfig = async () => {
-  const certPath = path.join(__dirname, 'certs', 'producao-630873-ecommerce-maps.p12');
-const cert = fs.readFileSync(certPath);
+  // Caminho correto para o certificado
+  const certPath = path.join(__dirname, '..', 'certs', 'producao-630873-ecommerce-maps.p12');
 
-  console.log(cert);
-  
+  // Ler o certificado
+  const cert = fs.readFileSync(certPath);
 
+  console.log(cert); 
+
+  // Configuração do cliente Gerencianet
   const gerenciaNetClient = new EfiPay({
     client_id: process.env.CHAVE_CLIENT_ID_PROD,
     client_secret: process.env.CHAVE_CLIENT_SECRET_PROD,
