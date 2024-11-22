7bdae305e11ce09e7243cec3ee59916c28508589
Guilherme <guissilval005@gmail.com>
2024-11-22 15:25:50 -0300

    Arrumando deploy backend2

--- Backend/config/gerencianet.js
+++ Backend/config/gerencianet.js
@@ -5,12 +5,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const gerenciaNetConfig = async () => {
-  const certificado = fs.readFileSync("certs/homologacao-630873-ecommerce-maps.p12");
+  const certificado = fs.readFileSync("certs/producao-630873-ecommerce-maps.p12");
 
   const gerenciaNetClient = new EfiPay({
     client_id: process.env.GN_CLIENT_ID,
     client_secret: process.env.GN_CLIENT_SECRET,
-    sandbox: process.env.GN_ENV === 'true', 
+    sandbox: process.env.GN_ENV === 'false', 
     certificate: certificado,
   });
 
