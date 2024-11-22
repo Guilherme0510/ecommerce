84840f76362ccb34e027fe3ae2e7b3427b36e4b0
Guilherme <guissilval005@gmail.com>
2024-11-22 16:55:13 -0300

    Arrumando o deploy do backend10

--- Backend/config/gerencianet.js
+++ Backend/config/gerencianet.js
@@ -1,23 +1,18 @@
 import EfiPay from "sdk-node-apis-efi";
-import fs from "fs";
 import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url"; // Para compatibilidade com módulos ES
 
 dotenv.config();
 
-// Obter o caminho do diretório com import.meta.url
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 export const gerenciaNetConfig = async () => {
-  // Usando a variável de ambiente CERT_PATH
-  const certPath = process.env.CERT_PATH || path.join(__dirname, "..", "certs", "producao-630873-ecommerce-maps.p12");
+  // Obter o conteúdo do certificado em Base64 da variável de ambiente
+  const certBase64 = process.env.CERTIFICATE_P12;
 
-  console.log("Caminho do certificado:", certPath);
+  if (!certBase64) {
+    throw new Error("Certificado não encontrado na variável de ambiente.");
+  }
 
-  // Ler o certificado
-  const cert = fs.readFileSync(certPath);
+  // Converter o Base64 para um Buffer
+  const cert = Buffer.from(certBase64, "base64");
 
   // Configuração do cliente Gerencianet
   const gerenciaNetClient = new EfiPay({
